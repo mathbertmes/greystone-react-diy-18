3a247386d246a1b6fc034617d3674ef9ab40c233
Matheus Bertemes <mathbertemes@gmail.com>
2024-10-16 20:41:39 -0700

    FORM

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from 'react';
+
 
 function App() {
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [chips, setChips] = useState(undefined)
+  const [specialRequest, setSpecialRequest] = useState("")
+  const [terms, setTerms] = useState(false)
+
+  const onSubmit = () => {
+    if(name && email && chips && terms){
+      alert(`Order name: ${name}! The email is: ${email}! Chips: ${chips}! ${specialRequest ? `Special request: ${specialRequest}!` : ""}`)
+    }else{
+      alert("required fields are empty!")
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="wrapperForm">
+        <div className="containerLabelAndInput">
+          <label >*Name: </label>
+          <input value={name} onChange={(e) => setName(e.target.value)} type="text" />
+        </div>
+        <div className='containerLabelAndInput'>
+          <label>*Email: </label>
+          <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" />
+        </div>
+        <div>
+          *
+          <select value={chips} onChange={(e) => setChips(e.target.value)}>
+            <option value={undefined}>--Do you want to order chips?--</option>
+            <option value={"No"}>No</option>
+            <option value={"Regular"}>Regular</option>
+            <option value={"Large"}>Large</option>
+          </select>
+        </div>  
+        <div className='containerLabelAndInput'>
+          <label>Special requests: </label>
+          <textarea value={specialRequest} onChange={(e) => setSpecialRequest(e.target.value)}></textarea>
+        </div>
+        <div>
+          <input className='termsCheckBox' value={terms} onChange={(e) => setTerms(e.target.value)}type="checkbox" />
+          <label>I have read and agreed to the terms and conditions</label>
+        </div>
+        <div className='containerSubmit'>
+          <button className='submitButton' onClick={onSubmit}>Make an order!</button>
+        </div>
+        </div>
     </div>
   );
 }
